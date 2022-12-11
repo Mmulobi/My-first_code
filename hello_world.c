df3f5ed53d54dce1ddf422b8651eb4b2deb05c51
Mmulobi <mosesmutush7@gmail.com>
2022-12-11 21:59:10 +0300

    my first hello world

---
+++ hello_world.c
@@ -0,0 +1,25 @@
+#inlude <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("Hello World");
+
+	return (0);
+}#inlude <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("Hello World");
+
+	return (0);
+}
